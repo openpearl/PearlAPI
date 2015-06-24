9bcc81b55034a790b4a7db283f20fbcfa850edc7
Mike <mike@goinvo.com>
2015-06-24 15:08:14 -0400

    Setting up routes to work with devise token auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
       resources :users, except: [:new, :edit]
-      patch 'documents/:id', to: 'documents#update', as: :document
-      post 'communications', to: 'communications#converse', as: :communicate
+      patch 'documents/:id'   => 'documents#update', as: :document
+      post 'communications'   => 'communications#converse', as: :communicate
+      # get    'login'          => 'sessions#new'
+      # post   'login'          => 'sessions#create'
+      # delete 'logout'         => 'sessions#destroy'
     end
   end
 end
