52d474b7d434242bd743ffb9afb5227c208e2f17
Mike <mike@goinvo.com>
2015-06-26 17:00:13 -0400

    Cleaned up routes
    Urls are now neater because they no longer require /api/v1 namespacing.
    A placeholder root is added which maps to a test action in application
    controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,15 @@ class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   include ActionController::MimeResponds
   include ActionController::StrongParameters
+  
   protect_from_forgery with: :null_session
   
   @@tvAccountID = ENV["TV_ACCOUNT_ID"]
   @@tvAdminAPI = ENV["TV_ADMIN_API_KEY"]
   @@tvVaultID = ENV["TV_VAULT_ID"]
   @@tvSchemaID = ENV["TV_SCHEMA_ID"]
+  
+  def test
+    render json: "placeholder root"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
+# Allows us to control which version of the api we are using.
+require 'api_constraints'
+
 Rails.application.routes.draw do
-  namespace :api, defaults: {format: 'json'} do
-    namespace :v1 do
+  scope module: :api, defaults: {format: 'json'} do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       mount_devise_token_auth_for 'User', at: 'auth'
       resources :users, except: [:new, :edit]
       patch 'documents/:id'   => 'documents#update', as: :document
       post 'communications'   => 'communications#converse', as: :communicate
-      # get    'login'          => 'sessions#new'
-      # post   'login'          => 'sessions#create'
-      # delete 'logout'         => 'sessions#destroy'
     end
   end
+  
+  root 'application#test'
 end
