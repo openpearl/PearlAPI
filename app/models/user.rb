d0add1dcd75b205e47f2cba63f6b843bfaa3ff37
Mike <mike@goinvo.com>
2015-06-30 18:05:27 -0400

    True vault documents are now initialized as an empty JSON

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,19 +10,16 @@ class User < ActiveRecord::Base
   #Setting up user attributes validation
   MAXIMUM_EMAIL_LENGTH = 255
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-  after_save {
-    
-  }
-
+  
   validates :email, presence: true, length: { maximum: MAXIMUM_EMAIL_LENGTH }, 
               format: { with: VALID_EMAIL_REGEX }
+ 
   
   #Given a user object and a TrueVault API key, returns a JSON array representing 
   #the user object created on TrueVault or an error if the user already exists
-  def create_tv_user(user_params, api_key)
-    email = user_params[:email].downcase 
-    password = user_params[:password]
+  def create_tv_user(sign_up_params, api_key)
+    email = sign_up_params[:email].downcase 
+    password = sign_up_params[:password]
     tvResponse = `curl https://api.truevault.com/v1/users   \
                   -X POST  -u #{api_key}:    \
                   -d "username=#{email}&password=#{password}"`
@@ -37,13 +34,11 @@ class User < ActiveRecord::Base
     tvresponse =  `curl https://api.truevault.com/v1/vaults/#{vault_id}/documents \
                   -u #{api_key}: \
                   -X POST \
-                  -d "document=ew0KICAgICJmaXJzdF9uYW1lIjogIiIsDQogICAgInN0ZXBzIjogIiINCn0=, schema_id = #{schema_id}"`
+                  -d "document=e30="`
     tvResponseJSON = JSON.parse(tvresponse)
     tvDocumentID = tvResponseJSON["document_id"]
     userID = user.id
-    tvDocument = Document.create(:documentID => tvDocumentID, :user_id => userID)
-        
+    tvDocument = Document.create(:documentID => tvDocumentID, :user_id => userID)    
   end
-
 
 end
