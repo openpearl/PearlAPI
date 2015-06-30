6a5c0169111b80fd16af3bc9e29bbcf9637d0e36
Mike <mike@goinvo.com>
2015-06-30 17:33:48 -0400

    Updated routes
    Registrations controller is now routing to the custom registrations
    controller instead of the devise default. Documents controller update
    route cleaned up - no longer requires an id in the uri.

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ require 'api_constraints'
 Rails.application.routes.draw do
   scope module: :api, defaults: {format: 'json'} do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      mount_devise_token_auth_for 'User', at: 'auth'
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations:  'api/v1/devise_controller_mod/registrations'
+      }
       resources :users, except: [:new, :edit]
-      patch 'documents/:id'   => 'documents#update', as: :document
+      patch 'documents'   => 'documents#update', as: :document
       post 'communications'   => 'communications#converse', as: :communicate
     end
   end
