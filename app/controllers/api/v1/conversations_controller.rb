194eddec2e3bb569eadbde202c634224ace804fc
Mike <zhihaochi@gmail.com>
2015-07-30 16:03:42 -0400

    Refactored Conversations Controller
    Moved out some code which should belong in the pearl engine

--- app/controllers/api/v1/conversations_controller.rb
+++ app/controllers/api/v1/conversations_controller.rb
@@ -4,59 +4,17 @@ class Api::V1::ConversationsController < ApplicationController
   before_action :authenticate_user!
 
 
+  # Returns a storyboard card at the cardID given if a plugin is loaded and initialized.
   def converse
-    convHash = @plugin.converse(context_params["cardID"], current_user.id)
-    #Checks to see if the conversation node is a leaf by checking whether or not
-    #it has children. If it does have children, then the conversation continues.
-    #If not, then we know that the conversation has come to an end.
-    if not convHash["childrenCardIDs"].nil?
-
-      # Filter out any invalid children
-      filteredCardIDs = []
-      convHash["childrenCardIDs"].each do |childCardID|
-        childCard = @plugin.converse(childCardID, current_user.id)
-        if not childCard["filters"].nil?
-          if @plugin.pass_filter?(childCard["filters"], current_user.id)
-            filteredCardIDs.push(childCardID)
-          end
-        else
-          filteredCardIDs.push(childCardID)
-        end
-      end
-
-
-      # If there is more than one valid child card after filtering, we choose a card
-      # randomly to proceed with in our conversation.
-      # We also pick an AI message randomly to add variety to the conversation.
-      numberOfchildren = filteredCardIDs.length
-      randomChild = Random.rand(numberOfchildren)
-      cardID = filteredCardIDs[randomChild]
-      childCard = @plugin.converse(cardID, current_user.id)
-
-      if childCard["messages"].class == Array
-        numberOfMessages = childCard["messages"].length
-        randomMessage = Random.rand(numberOfMessages)
-        childCard["messages"] = childCard["messages"][randomMessage]
-      end
-
-      # List of all the children cards of the chosen child card.
-      childrenArray = []
-      childCard["childrenCardIDs"].each do |nextCardID|
-        nextCard = @plugin.converse(nextCardID, current_user.id)
-        childrenArray.push(nextCard)
-      end
-
-      #Save the data about the children conversation nodes in the conversation hash under the
-      #key of "childrenCards".
-      childCard["childrenCards"] = childrenArray
-
-      #Renders a json of the conversation hash
-      render json: childCard
+    card = @plugin.getCard(context_params["cardID"], current_user.id)
+    if card.nil?
+      render json: {
+        status: "error",
+        message: 'End of conversation/no plugin loaded!'
+      }
     else
-      #Renders a null json since we are at a leaf node and there is no children.
-      render json: convHash["childrenCardIDs"]
+      render json: card
     end
-
   end
 
 
@@ -112,7 +70,6 @@ class Api::V1::ConversationsController < ApplicationController
         status: 'error',
         message: 'Document does not exist!'
       }
-
     end
   end
 
@@ -121,10 +78,8 @@ class Api::V1::ConversationsController < ApplicationController
 
   private
 
-  # Right now it is just picking move plugin by default. TODO: Make this smarter.
   def pick_plugin
-    @plugin ||= PearlEngine::Plugins::MovePlugin.new
-    @plugin.initializeConversation
+    @plugin ||= PearlEngine::PearlPlugin.choosePlugIn(current_user.id)
   end
 
   def get_document
